1c1a7844b661ea2ca4840ea06c6ed5beff9a92d2
Miguel Cataño <miangel5718@gmail.com>
2023-11-01 11:50:55 -0500

    migracion products

--- database/migrations/2023_11_01_164357_create_products_table.php
+++ database/migrations/2023_11_01_164357_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 80)->unique()->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('stock')->nullable();
+            $table->unsignedBigInteger('categories_id');
+            $table->foreign('categories_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
